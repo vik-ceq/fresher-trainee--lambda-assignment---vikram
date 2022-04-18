ba7bc1b5da6e205663ef3a436d443786cabfb5f8
vikram <100871985+vik-ceq@users.noreply.github.com>
2022-04-18 08:54:02 +0530

    Add files via upload

---
+++ output.tf
@@ -0,0 +1,19 @@
+output "arn" {
+  description = "ARN of the lambda function"
+  value       = local.enabled ? aws_lambda_function.this[0].arn : null
+}
+
+output "invoke_arn" {
+  description = "Inkoke ARN of the lambda function"
+  value       = local.enabled ? aws_lambda_function.this[0].invoke_arn : null
+}
+
+output "qualified_arn" {
+  description = "ARN identifying your Lambda Function Version (if versioning is enabled via publish = true)"
+  value       = local.enabled ? aws_lambda_function.this[0].qualified_arn : null
+}
+
+output "function_name" {
+  description = "Lambda function name"
+  value       = local.enabled ? aws_lambda_function.this[0].function_name : null
+}
